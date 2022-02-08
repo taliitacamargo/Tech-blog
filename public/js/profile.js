7724966cf0671e69d3fd01d404136770dbd0ca5d
Tali <taliitacamargo@icloud.com>
2022-02-08 13:59:39 -0500

    adding style

--- public/js/profile.js
+++ public/js/profile.js
@@ -22,26 +22,7 @@ const newFormHandler = async (event) => {
   }
 };
 
-// const delButtonHandler = async (event) => {
-//   if (event.target.hasAttribute('data-id')) {
-//     const id = event.target.getAttribute('data-id');
-
-//     const response = await fetch(`/api/posts/${id}`, {
-//       method: 'DELETE',
-//     });
-
-//     if (response.ok) {
-//       document.location.replace('/dashboard');
-//     } else {
-//       alert('Failed to delete post');
-//     }
-//   }
-// };
-
 document
   .querySelector('.new-project-form')
   .addEventListener('submit', newFormHandler);
 
-// document
-//   .querySelector('.project-list')
-//   .addEventListener('click', delButtonHandler);
